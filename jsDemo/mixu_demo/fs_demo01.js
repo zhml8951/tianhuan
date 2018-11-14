b611cc5df509c21d098b3ebde044d2c33b234d04
Arnni <zhml8951@126.com>
2018-11-14 22:44:15 +0800

    muxi nodejs demo

--- jsDemo/mixu_demo/fs_demo01.js
+++ jsDemo/mixu_demo/fs_demo01.js
@@ -1,11 +1,23 @@
 const fs = require('fs');
 fs.readFile('./dir_files', 'utf8', (err, data) =>{
 	console.log(data);
-	console.log(data.instance)
-	// console.log(data.toString());
 });
 
 
 setTimeout(()=>{
 	console.log('Timeout run at ' + new Date().toString());
 }, 1000);
+
+setTimeout(()=>{
+	console.log('Timeout run at ' + new Date().toString());
+}, 1000);
+
+var start = new Date();
+console.log('Enter loop at: ' + start.toTimeString());
+
+var i = 0;
+
+while(new Date().getTime() < start.getTime() + 2000) {
+	i++;
+}
+console.log('Exit  loop at: ' + new Date().toTimeString() + '. Ran ' +i+ ' iterations. ');
