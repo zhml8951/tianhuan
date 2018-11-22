33844a4249cf1fa47840c5af9954bac43a03d6c5
zhml8951 <zhml8951@126.com>
2018-11-22 09:30:21 +0800

    node js CommonJS AMD CMD

--- jsDemo/mixu_demo/fs_demo01.js
+++ jsDemo/mixu_demo/fs_demo01.js
@@ -3,7 +3,6 @@ fs.readFile('./dir_files', 'utf8', (err, data) =>{
 	console.log(data);
 });
 
-
 setTimeout(()=>{
 	console.log('Timeout run at ' + new Date().toString());
 }, 1000);
@@ -14,10 +13,10 @@ setTimeout(()=>{
 
 var start = new Date();
 console.log('Enter loop at: ' + start.toTimeString());
-
 var i = 0;
-
 while(new Date().getTime() < start.getTime() + 2000) {
 	i++;
 }
+
 console.log('Exit  loop at: ' + new Date().toTimeString() + '. Ran ' +i+ ' iterations. ');
+
