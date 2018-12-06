f92c6b6c074e0d805a7c51c6947c9528b4aec414
Arnni <zhml8951@126.com>
2018-12-06 22:06:33 +0800

    js set demo

--- jsDemo/small_demo/iter_demo02.js
+++ jsDemo/small_demo/iter_demo02.js
@@ -2,14 +2,14 @@ var str_iterate = () => {
 	const str01 = 'for...of vs for...of Loops in Javascript';
 	for (let c of str01) {
 		if(c !== ' '){
-		// console.log(c);
+		console.log(c);
 		}
 	}
 	for (let c in str01) {
-		// console.log(c);
+		console.log(c);
 	}
 }
-str_iterate();
+// str_iterate();
 
 function forOfTest() {
 	let animals = ['🐔', '🐷', '🐑', '🐇'];
@@ -20,7 +20,61 @@ function forOfTest() {
 	}
 }
 
-forOfTest();
+// forOfTest();
 
+function setDemo() {
+	let animals = new Set();
+	animals.add('🐷');
+	animals.add('🐼');
+	animals.add('🐢');
+	animals.add('🐿').add('O,MyGod!');
+	// animals.forEach(callbackfn, thisArg?)
+	console.log(animals.size);
+	console.log(animals);
+	animals.add('🐿');
+	console.log(animals.size);
+	console.log(animals.has('🐢'));
+	animals.delete('🐢');
+	console.log(animals.has('🐢'));
 
-https://alligator.io/js/
+	animals.forEach(a => {
+		console.log(`Hey, ${a}!!`);
+	});
+	animals.clear();
+	console.log(animals.size);
+
+	return animals;
+}
+
+// setDemo();
+console.log('-----------------------------------------------');
+function setDemo2(){
+	let myAnimals = new Set(['🐷', '🐢', '🐷', '🐷','🐑']);
+	myAnimals.add(['🐨','🐑']);
+	myAnimals.add({ name: 'Arnni', type: '🐑'});
+	// console.log(myAnimals)
+	myAnimals.forEach(animal => {
+		console.log(animal);
+	});
+
+	console.log('Only unique characters will be in this set.'.length);
+	let setString = new Set('Only unique characters will be in this set.');
+	console.log(setString.size);
+	console.log(setString);
+	setString.forEach(function(elem) {
+		console.log(elem);
+	});
+}
+// setDemo2();//
+(
+function setDemo3() {
+	let items;
+	let moreAnimals = new Set(['🐺', '🐴', '🐕', '🐇']);
+	for (let animal of moreAnimals) {
+			console.log(`Howdy ${ animal }`);
+	}
+	console.log(moreAnimals.entries());
+	console.log(moreAnimals.keys());
+	console.log(items = moreAnimals.values());
+	console.log(items.next().done);
+})();
