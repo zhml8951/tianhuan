205b4d56998a5846d414ed948744a259cce5c874
zhml8951 <zhml8951@126.com>
2018-12-20 08:13:48 +0800

    fun_demo02.js

--- jsDemo/small_demo/fun_demo02.js
+++ jsDemo/small_demo/fun_demo02.js
@@ -1,18 +1,45 @@
-var a = 0;
-function bb(x){
-	console.log(x);
+function fun_demo01() {
+	var a = 0;
+
+	function bb(x) {
+		console.log(x);
+	}
+
+	function timer(time, callback) {
+		setTimeout(function() {
+			a = 6;
+			callback(a);
+		}, time);
+	}
+
+	console.log(a);
+	timer(3000, bb);
+	timer(2000, function(x) {
+		console.log(x);
+	});
 }
 
-function timer(time, callback) {
-	setTimeout(function(){
-		a = 6;
-		callback(a);
-	}, time);
+function scope_demo01() {
+	var scope = "global scope";
+	function checkScope() {
+		var scope = "local scope";
+		function f() {
+			return scope;
+		}
+		return f();
+	}
+	console.log(checkScope());
 }
 
-console.log(a);
-timer(3000, bb);
-timer(2000, function(x){
-	console.log(x);
-});
-
+function scope_demo02() {
+	var scope = "global scope";
+	function checkScope(){
+		var scope = "local scope";
+		function f() {
+			return scope;
+		}
+		return f;
+	}
+	console.log(checkScope()());
+}
+scope_demo02();
