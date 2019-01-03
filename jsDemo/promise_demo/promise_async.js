449965dfa36594f5e644848df8a1e7353997ad83
zhml8951 <zhml8951@126.com>
2019-01-03 10:09:41 +0800

    js learn eslint add

--- jsDemo/promise_demo/promise_async.js
+++ jsDemo/promise_demo/promise_async.js
@@ -1,32 +1,33 @@
+/* eslint-disable no-unused-vars */
 var count = () =>{
-	return new Promise( (resolve, reject) => {
-		setTimeout( () => {
-			resolve(100);
-		}, 500);
-	});
-}
+    return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            resolve(100);
+        }, 500);
+    });
+};
 
 var list = () => {
-	return new Promise( (resolve, reject) => {
-		setTimeout( ()=>{
-			resolve([1,2,3]);
-		}, 500);
-	});
-}
+    return new Promise( (resolve, reject) => {
+        setTimeout( ()=>{
+            resolve([1,2,3]);
+        }, 500);
+    });
+};
 
 var getList = async () => {
-	let result = await Promise.all([count(), list()]);
-	return result;
-}
+    let result = await Promise.all([count(), list()]);
+    return result;
+};
 
 console.time('begin');
 
 getList().then(result => {
-	console.timeEnd('begin');
-	console.log(result);
+    console.timeEnd('begin');
+    console.log(result);
 }).catch(err => {
-	console.timeEnd('begin');
-	console.log(err);
+    console.timeEnd('begin');
+    console.log(err);
 });
 
 
